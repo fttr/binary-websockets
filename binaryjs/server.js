a3fb902a61c6d77e66a35faf3de4fab593a2301e
André Futter <a.futter@bigpoint.net>
2014-11-14 10:27:02 +0100

    added some example code

---
+++ binaryjs/server.js
@@ -0,0 +1,10 @@
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
+// Start Binary.js server
+var server = BinaryServer({port: 9000});
+// Wait for new user connections
+server.on('connection', function(client){
+// Stream a flower as a hello!
+    var file = fs.createReadStream('../assets/flower.png');
+    client.send(file);
+});
