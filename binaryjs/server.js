d3ad11fea35180e371faf1824081e85ff250b81f
fttr <a.futter@gmail.com>
2014-11-15 16:33:32 +0100

    added broadcast

--- binaryjs/server.js
+++ binaryjs/server.js
@@ -1,5 +1,8 @@
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
+
+var clients = [];
+
 // Start Binary.js server
 var server = BinaryServer({port: process.env.PORT});
 
@@ -8,28 +11,34 @@ console.log(process.env.PORT);
 // Wait for new user connections
 server.on('connection', function(client) {
 
+    clients.push(client);
+
+
     client.on('stream', function(stream, meta) {
 
-        // Client requests image by reference
+
         stream.on('data', function(data){
 
-            //console.log('got data:', data);
+            // Client sends image as blob
 
-            if (data.requestImage) {
-                var whitelist = ['flower', 'space'];
-                var ref = data.requestImage;
+            console.log('got blob');
 
-                if (whitelist.indexOf(ref) !== -1) {
-                    var file = getImageStream(ref);
-                    client.send(file);
-                }
-            } else {
-                console.log('got blob');
+            var blob = new Buffer(data, "binary");
 
-                var blob = new Buffer(data, "binary");
-                client.send(blob);
+            // Broadcast image to all connected clients
+            for (var client in clients) {
+                clients[client].send(blob);
             }
         });
+    });
+
+    client.on('close', function () {
+        for (var i = 0; i < clients.length; i++) {
+            if (clients[i].id == client.id) {
+                clients.splice(i, 1);
+            }
+        }
+        client.close();
     });
 });
 
